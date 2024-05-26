05c160ce85640975f9abcf4b5cf7e2e96ac39281
GaspardRuan <1039553124@qq.com>
2024-05-26 16:32:07 +0800

    feat: add countdown timer

---
+++ global-top.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import VueCountdown from '@chenfengyuan/vue-countdown';
+const ready = ref(false);
+
+const start = () => {
+  ready.value = true;
+};
+
+</script>
+
+<template>
+<div class="absolute top-0 left-0">
+  <button type="button" v-if="!ready" @click="start" class="text-red-700 px-3 py-2 text-xs font-medium text-center ">Start Timer</button>
+  <vue-countdown class="px-1 py-1 text-xs font-medium" v-if="ready" :time="1 * 1 * 10 * 60 * 1000" v-slot="{ minutes, seconds }">
+    {{ minutes }}:{{ seconds }}
+  </vue-countdown>
+</div>
+
+</template>
